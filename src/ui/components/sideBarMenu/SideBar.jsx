4892becab85d00466398d4360a4d19b69101bcc5
MiguelDomingues-Dev <miguelcamargoadomingues@gmail.com>
2025-03-31 11:54:17 -0300

    .

--- src/ui/components/sideBarMenu/SideBar.jsx
+++ src/ui/components/sideBarMenu/SideBar.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import './sideBar.css';
-import Header from './header/header';
+import Header from './header/Header';
 import MenuBar from './menu/MenuBar';
 import FooterSideBar from './footer/Footer';
 
