696fee635377b7d05c656d4685eb493b8ab38318
MiguelDomingues-Dev <miguelcamargoadomingues@gmail.com>
2025-04-01 00:48:20 -0300

    Ajustando a rota para a landingpage

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import ProtectedRoute from "./routes/ProtectedRoute";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Resgiter";
@@ -9,6 +9,9 @@ export default function AppRoutes() {
     return (
         <BrowserRouter>
             <Routes>
+                {/* Rota padrão que redireciona para a Landinpage */}
+                <Route path="/" element={<Navigate to="/landinpage" />} />
+                
                 <Route path="/landinpage" element={<Landinpage />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
