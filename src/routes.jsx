e60da13196dc8fb7c41fd42d9ee046f585fbbe94
MiguelDomingues-Dev <miguelcamargoadomingues@gmail.com>
2025-04-01 00:49:48 -0300

    ajustando pequenas falhas

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,7 +3,7 @@ import ProtectedRoute from "./routes/ProtectedRoute";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Resgiter";
 import Overview from "./pages/Overview/overview";   
-import Landinpage from "./pages/Landinpage/landigpage";
+import Landinpage from "./pages/Landinpage/Landigpage";
 
 export default function AppRoutes() {
     return (
