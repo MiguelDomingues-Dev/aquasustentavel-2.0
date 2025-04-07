1dd8099dd71935be5e0f9199b19052f5de7d59b1
MiguelDomingues-Dev <miguelcamargoadomingues@gmail.com>
2025-04-07 00:28:36 -0300

    implementação do logout

--- src/ui/components/sideBarMenu/footer/Footer.jsx
+++ src/ui/components/sideBarMenu/footer/Footer.jsx
@@ -1,13 +1,26 @@
-import * as React from 'react';
+import React, { useContext } from "react";
 import { IoIosLogOut } from "react-icons/io";
 import './footer.css';
+import { AuthContext } from "../../../../services/AuthContext";
 
 export default function FooterSideBar() {
-    return(
+    const { logout } = useContext(AuthContext);
+
+    const handleLogout = async () => {
+        try {
+            await logout();
+        } catch (error) {
+            console.error("Erro ao deslogar:", error);
+        }
+    };
+
+    return (
         <div className='footer'>
             <p>
                 <IoIosLogOut 
                     cursor="pointer"
+                    onClick={handleLogout}
+                    style={{ color: "red", fontSize: "24px" }} // Corrigido o uso de 'sx' para 'style'
                 />
             </p>
         </div>
